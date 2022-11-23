206ab5df39f2411b62fab2ed891209d4f0433ee6
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-11-23 19:36:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+//Created by Timothy Manwell
+// Created on: Nov 2022
+//
+// This program sees what movies you can watch based on your age
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int age;
+      
+        Console.WriteLine("What Movies Can You Watch?");
+        Console.Write("\nEnter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        if (age > 13)
+        {
+            Console.WriteLine("\nYou can watch R rated movies.");
+        }
+        else if (age == 13)
+        {
+            Console.WriteLine("\nYou can watch pg13 rated movies");
+        }
+        else if (age < 13)
+        {
+            Console.WriteLine("\nYou can watch G rated movies.");
+        }
+    }
+}
